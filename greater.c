dfeae0fba7d81e9f49e3e697bb4936a9b548f23d
Shakshijain <35430851+Shakshijain@users.noreply.github.com>
2018-01-21 06:04:26 -0800

    Add files via upload

---
+++ greater.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("enter the 3 number");
+scanf("%d%d%d",&a,&b,&c);
+if(a<b && b>c)
+{
+	printf("%d",b);
+}
+else if(a<c && c>b)
+{
+	printf("%d",c);
+}
+else if(a>b && a>c)
+{
+	printf("%d",a);
+	
+}
+else
+{
+	printf("you are take a negative value");
+}
+	
+}
