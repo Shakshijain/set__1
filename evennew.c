41b15d3b42a2057e090281bb48e619df3158e5ea
Shakshijain <35430851+Shakshijain@users.noreply.github.com>
2018-01-22 20:17:59 -0800

    Add files via upload

---
+++ evennew.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int n,i;
+	printf("enter the number to check no is even or odd");
+	scanf("%d",&n);
+	if(n/2==0)
+	{
+		printf("no is even");
+	}
+	else
+	printf("no is even");
+	
+    }
+	
