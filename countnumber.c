f5fc5b752021b1a6fa9e9f2600ed9ad85728659f
Shakshijain <35430851+Shakshijain@users.noreply.github.com>
2018-01-27 09:08:17 -0800

    Add files via upload

---
+++ countnumber.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main() 
+{
+    int n,c=0;
+    printf("enter the number");
+    scanf("%d",&n);
+    while(n!=0)
+    {
+    	c=c+1;	
+    n=n/10;
+	
+	}
+	printf("%d",c);
+}
